c9443d2c6f52daf5ff5b20a2bb202523cb85cf02
ysrLand <ysrLand1418@gmail.com>
2021-05-15 15:41:29 +0900

    Animation

--- app/src/main/java/com/ysr/learn/gugudan/quiz/QuizFragment.kt
+++ app/src/main/java/com/ysr/learn/gugudan/quiz/QuizFragment.kt
@@ -1,14 +1,15 @@
 package com.ysr.learn.gugudan.quiz
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.GridLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,6 +20,8 @@ import java.lang.StringBuilder
 private val TAG = QuizFragment::class.simpleName
 private const val QUIZ_TABLE = "quiz_table"
 private const val questionSet = 9
+private const val OK = -2
+private const val BACK = -1
 
 class QuizFragment : Fragment() {
     private var table: Int = 0
@@ -46,7 +49,7 @@ class QuizFragment : Fragment() {
     ): View? {
         val columnCount = 3
         val view = inflater.inflate(R.layout.fragment_quiz, container, false)
-        val equationView = view.findViewById<TextView>(R.id.eval)
+        val equationView = view.findViewById<TextView>(R.id.equation)
         val gridView = view.findViewById<GridLayout>(R.id.keypad)
         val progressView = view.findViewById<TextView>(R.id.progress)
         val viewModelStore = requireActivity().viewModelStore
@@ -72,29 +75,71 @@ class QuizFragment : Fragment() {
         with(viewModel) {
             digit.observe(viewLifecycleOwner, Observer {
                 when (it) {
-                    -2 -> {
+                    OK -> {
                         if (answer == multiplicand * multiplier) {
                             if (step == multipliers.size) {
-                                showToast(getString(R.string.clear, this@QuizFragment.table))
-                                removeThis()
+                                AlertDialog.Builder(context)
+                                    .setMessage(getString(R.string.clear, this@QuizFragment.table))
+                                    .setPositiveButton(
+                                        android.R.string.ok
+                                    ) { _, _ -> removeThis() }
+                                    .show()
                             } else {
-                                showToast(getString(R.string.correct))
-                                generateEquation()
-                                updateProgress(progressView)
-                                putEquation(equationView)
+                                val animation =
+                                    AnimationUtils.loadAnimation(context, R.anim.disappear)
+                                animation.setAnimationListener(object :
+                                    Animation.AnimationListener {
+                                    override fun onAnimationRepeat(p0: Animation?) {
+                                    }
+
+                                    override fun onAnimationEnd(p0: Animation?) {
+                                        generateEquation()
+                                        updateProgress(progressView)
+                                        putEquation(equationView)
+                                        equationView.startAnimation(
+                                            AnimationUtils.loadAnimation(
+                                                context,
+                                                R.anim.appear
+                                            )
+                                        )
+                                    }
+
+                                    override fun onAnimationStart(p0: Animation?) {
+                                    }
+                                })
+                                equationView.startAnimation(animation)
                             }
                         } else {
-                            showToast(getString(R.string.wrong))
-                            putEquation(equationView)
+                            val animation = AnimationUtils.loadAnimation(context, R.anim.wrong)
+                            animation.setAnimationListener(object : Animation.AnimationListener {
+                                override fun onAnimationRepeat(p0: Animation?) {
+
+                                }
+
+                                override fun onAnimationEnd(p0: Animation?) {
+                                    equationView.startAnimation(
+                                        AnimationUtils.loadAnimation(
+                                            context,
+                                            R.anim.wrong
+                                        )
+                                    )
+                                    putEquation(equationView)
+                                }
+
+                                override fun onAnimationStart(p0: Animation?) {
+
+                                }
+                            })
+                            equationView.startAnimation(animation)
                         }
                         clearAnswer()
                     }
-                    -1 -> {
+                    BACK -> {
                         answer /= 10
                         putEquation(equationView, answer)
                     }
                     else -> {
-                        answer = answer * 10 + it
+                        answer = (answer * 10 + it) % 100
                         putEquation(equationView, answer)
                     }
                 }
@@ -117,7 +162,7 @@ class QuizFragment : Fragment() {
     private fun generateEquation() {
         multiplier = try {
             multipliers[step++]
-        } catch (e:IndexOutOfBoundsException) {
+        } catch (e: IndexOutOfBoundsException) {
             0
         }
     }
@@ -139,14 +184,6 @@ class QuizFragment : Fragment() {
                     ""
                 }
             )
-    }
-
-    private fun showToast(message: String) {
-        val toast = Toast.makeText(context, message, Toast.LENGTH_SHORT)
-        toast.setGravity(Gravity.CENTER, 0, 0)
-        //TODO: fix not toast but popup
-        ((toast.view as ViewGroup).getChildAt(0) as TextView).setTextAppearance(R.style.DefaultTextAppearance)
-        toast.show()
     }
 
     private fun clearAnswer() {
